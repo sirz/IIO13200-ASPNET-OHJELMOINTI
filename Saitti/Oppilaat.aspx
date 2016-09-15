9b4042fca6f33788787c3c160f0001b157ff5a3a
sirz <sirzmur@gmail.com>
2016-09-15 18:31:31 +0300

    15.9 tunnit

---
+++ Saitti/Oppilaat.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Oppilaat.aspx.cs" Inherits="Oppilaat" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <h1>Oppilaat syksy 2016</h1>
+        <div id="Puttoset">
+            <asp:Button ID="btnGet3" runat="server" Text="Haje 3 oppilasta" OnClick="btnGet3_Click" />
+            <asp:Button ID="btnGetAll" runat="server" Text="Haje oppilaat tietokannasta" OnClick="btnGetAll_Click" />
+            <asp:Button ID="btnGet3Oppilas" runat="server" Text="Haje 4 oppilasoliota" OnClick="btnGet3Oppilas_Click" />
+        </div>
+        <div id="data">
+            <asp:GridView ID="gvStudents" runat="server"></asp:GridView>
+        </div>
+        <div id="footer">
+             <asp:Label ID="lblMessages" runat="server" ></asp:Label> 
+            </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Oppilaat.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data; //ADO.NETin kaikki data-perusluokat ml. datatable
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Oppilaat : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnGet3_Click(object sender, EventArgs e)
+    {
+        //haetaan valmiilta luokan metodilta dataa ja kiinnitetään se kontrolliin
+        DataTable dt = JAMK.ICT.Data.DBPlacebo.Get3TestStudents();
+        gvStudents.DataSource = dt;
+        gvStudents.DataBind();
+    }
+
+    protected void btnGetAll_Click(object sender, EventArgs e)
+    {
+        string cs = System.Configuration.ConfigurationManager.
+            ConnectionStrings["oppilaat"].ConnectionString;
+        string messu = "";
+        try
+        {
+            DataTable dt = JAMK.ICT.Data.DBPlacebo.GetAllStudentsFromSQLServer(cs, "oppilaat", out messu);
+            gvStudents.DataSource = dt;
+            gvStudents.DataBind();
+        }
+        catch (Exception ex)
+        {
+            //mihin kirjoitetaan poikkeus messu?
+            lblMessages.Text = ex.Message;
+            
+        }
+    }
+
+    protected void btnGet3Oppilas_Click(object sender, EventArgs e)
+    {
+        //haetaan valmista businesslogiikasta oppilas-olioita ja 
+        //kiinnitetään ne kontrollin datasourceksi
+        var oppilaat = new JAMK.ICT.BL.Oppilaat().Hae3TestiOppilasta();
+        gvStudents.DataSource = oppilaat;
+        gvStudents.DataBind();
+    }
+}
