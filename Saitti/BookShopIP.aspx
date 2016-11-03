75cb3c4d04a4df6e579dc72ff560b98902b49d0c
salesa <salesa@MEDI-426-WS-00>
2016-11-03 18:56:08 +0200

    EntityFramework Demon eka osio tehty

---
+++ Saitti/BookShopIP.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageIP.master" AutoEventWireup="true" CodeFile="BookShopIP.aspx.cs" Inherits="BookShopIP" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
+    <h1>Esan KirjakauppaX</h1>
+    <div class="w3-row">
+        <!-- vasen lohko: asiakkaat -->
+        <div class="w3-container w3-third">
+            <asp:Button ID="btnGetCustomers" runat="server" 
+                text="Hae asiakkaat"
+                CssClass="w3-btn" OnClick="btnGetCustomers_Click" />
+            <asp:DropDownList ID="ddlCustomers" runat="server" OnSelectedIndexChanged="ddlCustomers_SelectedIndexChanged" AutoPostBack="True" />
+            <asp:GridView ID="gvCustomers" runat="server" />
+        </div>
+        <!-- oikea lohko: kirjat -->
+        <div class="w3-container w3-twothird">
+            <asp:Button ID="btnGetBooks" runat="server" 
+                text="Hae kirjat"
+                CssClass="w3-btn w3-blue" OnClick="btnGetBooks_Click" />
+            <asp:GridView ID="gvBooks" runat="server" />
+
+        </div>
+    </div>
+    <div class="w3-row">
+        <asp:Literal ID="ltResult" runat="server" />
+    </div>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="footer" Runat="Server">
+    <asp:Label ID="lblMessages" runat="server" />
+</asp:Content>
+
---
+++ Saitti/BookShopIP.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class BookShopIP : System.Web.UI.Page
+{
+    protected static BookShopEntities ctx;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //ctx:ään tiedot vain kerran ja kontrollien populointi
+        if (!IsPostBack)
+        {
+            ctx = new BookShopEntities();
+            FillControls();
+        }
+    }
+    #region METHODS
+    protected void FillControls()
+    {
+        //dropdownlist
+        //var retval = ctx.Customers;
+        //var retval = from c in ctx.Customers
+        //             orderby c.lastname
+        //             select c;
+        //sama lambda tyylillä
+        var retval = ctx.Customers.OrderBy(x => x.lastname);
+        ddlCustomers.DataSource = retval.ToList();
+        ddlCustomers.DataTextField = "lastname";
+        ddlCustomers.DataValueField = "id";
+        ddlCustomers.DataBind();
+        //listään tyhjä alkio ddl:ään
+        ddlCustomers.Items.Insert(0, string.Empty);
+    }
+    protected void ShowCustomers()
+    {
+        gvCustomers.DataSource = ctx.Customers.ToList();
+        gvCustomers.DataBind();
+        int i = ctx.Customers.Count();
+        lblMessages.Text = string.Format("Haettu {0} asiakkaan tiedot", i);
+    }
+    protected void ShowBooks()
+    {
+        gvBooks.DataSource = ctx.Books.ToList();
+        gvBooks.DataBind();
+        int i = ctx.Books.Count();
+        lblMessages.Text = string.Format("Haettu {0} kirjan tiedot", i);
+    }
+    protected void ShowCustomersOrders(Customer kusti)
+    {
+        //näytetään asiakkaan tilaukset ja tilaukseen kuuluvat rivit=kirjat
+        ltResult.Text = string.Format("Asiakkaalla {0} {1} on {2} tilausta",
+            kusti.firstname, kusti.lastname, kusti.Orders.Count);
+        //loopitetaan asiakkaan tilaukset läpi
+        foreach (var o in kusti.Orders)
+        {
+            ltResult.Text  += 
+                string.Format("<br>- tilaus {0} sisältää {1} kirjaa:", 
+                o.odate, o.Orderitems.Count());
+            //loopitetaann tilauksen tilausrivit
+            foreach (var oi in o.Orderitems)
+            {
+                ltResult.Text +=
+                    string.Format("<br>-- kirjaa {0} {1} kpl {2}",
+                    oi.Book.name, oi.Book.author, oi.count);
+            }
+        }
+    }
+    #endregion
+    protected void btnGetCustomers_Click(object sender, EventArgs e)
+    {
+        ShowCustomers();
+    }
+
+    protected void btnGetBooks_Click(object sender, EventArgs e)
+    {
+        ShowBooks();
+    }
+
+    protected void ddlCustomers_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (ddlCustomers.SelectedIndex > 0)
+        { 
+        //valitun asiakkaan indeksin perusteella
+        int id = int.Parse(ddlCustomers.SelectedValue);
+        var ret = from c in ctx.Customers
+                  where c.id == id
+                  select c;
+        Customer kusti = ret.FirstOrDefault();
+        ShowCustomersOrders(kusti);
+        }
+    }
+}
